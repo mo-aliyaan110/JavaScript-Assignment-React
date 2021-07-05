cf5db4af6c0f5e8fa066e99f02abde920a820185
mo-aliyaan110 <mohammed007aliyan@gmail.com>
2021-07-05 22:37:59 +0530

    initial

---
+++ vote.js
@@ -0,0 +1,24 @@
+let voteCounting1 = 0;
+function vote1(){
+    
+    let voting = document.getElementById('votecount1');
+    voting.innerText = ++voteCounting1;
+}
+let voteCounting2 = 0;
+function vote2(){
+    
+    let voting = document.getElementById('votecount2');
+    voting.innerText = ++voteCounting2;
+}
+let voteCounting3 = 0;
+function vote3(){
+    
+    let voting = document.getElementById('votecount3');
+    voting.innerText = ++voteCounting3;
+}
+let voteCounting4 = 0;
+function vote4(){
+    
+    let voting = document.getElementById('votecount4');
+    voting.innerText = ++voteCounting4;
+}
