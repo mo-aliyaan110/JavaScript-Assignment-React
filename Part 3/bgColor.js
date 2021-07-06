abfedc653cf2207b81e3dee090f68ea7c5ac677d
mo-aliyaan110 <mohammed007aliyan@gmail.com>
2021-07-06 10:30:23 +0530

    done

---
+++ Part 3/bgColor.js
@@ -0,0 +1,8 @@
+function changeBG(color){
+    document.getElementById('parent').style.backgroundColor = color;
+
+    // document.children.style.background = color;
+    // // document.firstElementChild.style.background = color;
+    // document.childNodes(1).style.background = color;
+}
+    
